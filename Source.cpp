1d3195d9bb4a5f58dc70e6cb5176a04afb592000
Vamdawn <vamdawn@outlook.com>
2016-03-02 19:09:09 +0800

    delete the useless notes

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,4 @@
 #include<Windows.h>
-//#pragma comment(linker,"/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 #include<iostream>
 #include<stdlib.h>
 
@@ -7,23 +6,6 @@ using namespace std;
 
 int main()
 {
-    /*HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    // 获取标准输出设备句柄
-    CONSOLE_SCREEN_BUFFER_INFO bInfo; // 窗口缓冲区信息
-    GetConsoleScreenBufferInfo(hOut, &bInfo);
-    // 获取窗口缓冲区信息
-    char strTitle[255];
-    GetConsoleTitle(strTitle, 255); // 获取窗口标题
-    printf("当前窗口标题是：%s\n", strTitle);
-    SetConsoleTitle("控制台窗口操作"); // 设置窗口标题
-    COORD size = { 80, 25 };
-    SetConsoleScreenBufferSize(hOut, size); // 重新设置缓冲区大小
-    SMALL_RECT rc = { 0, 0, 80 - 1, 25 - 1 }; // 重置窗口位置和大小
-    SetConsoleWindowInfo(hOut, true, &rc);
-    getchar();*/
-    
-    //system("START /B /WAIT MineBreaker.exe");
-    //WinExec("cmd.exe", SW_HIDE);
     long pixel;
     char h[30];
     int tt[8][8];
@@ -64,7 +46,6 @@ int main()
     }
    
     DeleteDC(A);
-    //CloseHandle(hOut); // 关闭标准输出设备句柄
     system("pause");
 
     return 0;
